f07b565adc8668581d88e74e756ca9988e336263
Anthony <anthony.gilbert93@gmail.com>
2021-11-23 21:08:55 -0600

    156. Playlist Details Template: Added styling to the PlaulistDetails view

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="error"></div>
-  <div v-if="document" class="playlist-details">
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
     <div class="playlist-info">
       <div class="cover">
-        <p>Image goes here</p>
-        <img :src="document.coverURL" alt="" />
+        <img :src="playlist.coverURL" alt="" />
       </div>
-      <p>Playlist Details:</p>
-      <p>Path: {{ path }}</p>
-      <p>ID: {{ id }}</p>
+      <h2>{{ playlist.title }}</h2>
+
+      <p class="username">Created by: {{ playlist.userName }}</p>
+      <p class="description">Description: {{ playlist.description }}</p>
+    </div>
+    <!-- song list -->
+    <div class="song-list">
+      <p>song list here</p>
     </div>
   </div>
 </template>
@@ -22,12 +26,12 @@ export default {
   props: ["id"],
   setup(props) {
     const route = useRoute();
-    const { error, document } = getDocument("playlist", props.id);
+    const { error, document: playlist } = getDocument("playlist", props.id);
     console.clear();
 
-    console.log("document: ", document);
-    watchEffect(() => console.log(document));
-    console.log("document.title: ", document.title);
+    console.log("playlist: ", playlist);
+    watchEffect(() => console.log(playlist));
+    console.log("playlist.title: ", playlist.title);
 
     // let myTarget = JSON.parse(JSON.stringify(document._rawValue.coverURL));
 
@@ -36,10 +40,48 @@ export default {
     console.log("route: ", route.path);
     console.log("route: ", route);
     const path = route.path;
-    return { path, document };
+    return { path, playlist };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
 </style>
