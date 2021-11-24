79e5d3a3adc0ad20dc88c710588b62886bd767cb
Anthony <anthony.gilbert93@gmail.com>
2021-11-23 20:47:42 -0600

    Feature: Displayed image cover on corresponding playlist pages

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,21 +1,42 @@
 <template>
-  <div>
-    <p>Playlist Details:</p>
-    <p>Path: {{ path }}</p>
-    <p>ID: {{ id }}</p>
+  <div class="error"></div>
+  <div v-if="document" class="playlist-details">
+    <div class="playlist-info">
+      <div class="cover">
+        <p>Image goes here</p>
+        <img :src="document.coverURL" alt="" />
+      </div>
+      <p>Playlist Details:</p>
+      <p>Path: {{ path }}</p>
+      <p>ID: {{ id }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { useRouter, useRoute } from "vue-router";
+import { watchEffect } from "vue";
+import { useRoute } from "vue-router";
+import getDocument from "@/composables/getDocument.js";
 
 export default {
   props: ["id"],
-  setup() {
+  setup(props) {
     const route = useRoute();
+    const { error, document } = getDocument("playlist", props.id);
+    console.clear();
+
+    console.log("document: ", document);
+    watchEffect(() => console.log(document));
+    console.log("document.title: ", document.title);
+
+    // let myTarget = JSON.parse(JSON.stringify(document._rawValue.coverURL));
+
+    // console.log("document._rawValue.coverURL: ", myTarget);
+
     console.log("route: ", route.path);
+    console.log("route: ", route);
     const path = route.path;
-    return { path };
+    return { path, document };
   },
 };
 </script>
