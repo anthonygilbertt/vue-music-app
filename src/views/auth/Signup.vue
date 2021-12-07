017ff647adfe1c70022c1bb121c1f293422b5ead
anthonygilbert <anthony.gilbert93@gmail.com>
2021-12-07 12:44:03 -0600

    Used useRouter to push the user to the home page upon signup

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -20,12 +20,13 @@
 
 <script>
 import useSignup from "@/composables/useSignup.js";
+import { useRouter  } from "vue-router";
 import { ref } from "vue";
 
 export default {
   setup() {
     const { error, signup, isPending } = useSignup();
-
+const router = useRouter();
     const displayName = ref("");
     const email = ref("");
     const password = ref("");
@@ -34,6 +35,7 @@ export default {
       const res = await signup(email.value, password.value, displayName.value);
       if (!error.value) {
         console.log("user signed up in");
+        router.push({ name: 'Home' })
       }
     };
 
